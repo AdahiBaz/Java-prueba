ea64f38ba1933a8e9f41885efbb302785c0e5320
AdahiBaz <74736394+AdahiBaz@users.noreply.github.com>
2022-09-12 18:50:40 +0200

    Adahi-Sin coma

--- Java 1/src/Java.java
+++ Java 1/src/Java.java
@@ -3,7 +3,7 @@ public class Java {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hola mundo, estoy de vuelta");
+		System.out.println("Hola mundo estoy de vuelta");
 
 	}
 
